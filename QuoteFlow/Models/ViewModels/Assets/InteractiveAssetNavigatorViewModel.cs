14ad4649415d6bceea712dd41dc5f6d65a9b26db
Josh Sullivan <jaysche.sullivan@gmail.com>
2014-06-28 14:11:16 -0400

    Catalog interactive assets list page now has a full list of all the assets

---
+++ QuoteFlow/Models/ViewModels/Assets/InteractiveAssetNavigatorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace QuoteFlow.Models.ViewModels.Assets
+{
+    public class InteractiveAssetNavigatorViewModel
+    {
+        public AssetDetailsModel CurrentAssetDetailsModel { get; set; }
+
+        public IEnumerable<Asset> Assets { get; set; } 
+    }
+}
