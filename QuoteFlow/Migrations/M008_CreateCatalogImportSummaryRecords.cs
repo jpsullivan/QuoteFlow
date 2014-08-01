7a7e82a6e3d013d0c01360d5dbb7fc1b856ca692
Josh Sullivan <jaysche.sullivan@gmail.com>
2014-08-01 18:20:33 -0400

    Added Assets and Import summary table migrations

---
+++ QuoteFlow/Migrations/M008_CreateCatalogImportSummaryRecords.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+
+namespace QuoteFlow.Migrations
+{
+    [Migration(201408011807)]
+    public class M008_CreateCatalogImportSummaryRecords : Migration
+    {
+        private const string TableName = "CatalogImportSummaryRecords";
+
+        public override void Up()
+        {
+            Create.Table(TableName)
+                .WithColumn("Id").AsInt32().NotNullable().Identity().PrimaryKey()
+                .WithColumn("CatalogId").AsInt32().NotNullable()
+                .WithColumn("Result").AsString(15).NotNullable()
+                .WithColumn("RowId").AsInt32().NotNullable()
+                .WithColumn("AssetId").AsInt32().Nullable()
+                .WithColumn("Reason").AsString(350).Nullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table(TableName);
+        }
+    }
+}
---
+++ QuoteFlow/Migrations/M009_CreateAssets.cs
@@ -0,0 +1,33 @@
+﻿using FluentMigrator;
+using QuoteFlow.Infrastructure.Extensions;
+
+namespace QuoteFlow.Migrations
+{
+    [Migration(201408011810)]
+    public class M009_CreateAssets : Migration
+    {
+        private const string TableName = "Assets";
+
+        public override void Up()
+        {
+            Create.Table(TableName)
+                .WithColumn("Id").AsInt32().NotNullable().Identity().PrimaryKey()
+                .WithColumn("Name").AsString(250).NotNullable()
+                .WithColumn("SKU").AsString(150).NotNullable()
+                .WithColumn("Type").AsString(50).NotNullable()
+                .WithColumn("Description").AsMaxString().Nullable()
+                .WithColumn("Cost").AsCurrency().NotNullable()
+                .WithColumn("Markup").AsDecimal(9, 2).Nullable()
+                .WithColumn("ManufacturerId").AsInt32().Nullable()
+                .WithColumn("CatalogId").AsInt32().NotNullable()
+                .WithColumn("CreatorId").AsInt32().NotNullable()
+                .WithColumn("LastUpdated").AsDateTime().NotNullable()
+                .WithColumn("CreationDate").AsDateTime().NotNullable();
+        }
+
+        public override void Down()
+        {
+            Delete.Table(TableName);
+        }
+    }
+}
