f57af0677e4087fe6adbfcd4edb79d5acac02c4f
Josh Sullivan <jaysche.sullivan@gmail.com>
2014-08-01 16:57:28 -0400

    Created initial migration (Users table) and a migrator executable

---
+++ QuoteFlow/Migrations/M001_CreateUsers.cs
@@ -0,0 +1,30 @@
+﻿using FluentMigrator;
+
+namespace QuoteFlow.Migrations
+{
+    [Migration(201408011558)]
+    public class M001_CreateUsers : Migration
+    {
+        private const string TableName = "Users";
+
+        public override void Up()
+        {
+            Create.Table(TableName)
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                .WithColumn("Username").AsString(64).NotNullable()
+                .WithColumn("EmailAddress").AsString(256).Nullable()
+                .WithColumn("UnconfirmedEmailAddress").AsString(256).Nullable()
+                .WithColumn("FullName").AsString(100).Nullable()
+                .WithColumn("IsAdmin").AsBoolean().NotNullable().WithDefaultValue(false)
+                .WithColumn("EmailAllowed").AsBoolean().NotNullable()
+                .WithColumn("EmailConfirmationToken").AsString(32).Nullable()
+                .WithColumn("PasswordResetToken").AsString(32).Nullable()
+                .WithColumn("PasswordResetTokenExpirationDate").AsDateTime().Nullable()
+                .WithColumn("CreatedUtc").AsDateTime().Nullable()
+                .WithColumn("LastLoginUtc").AsDateTime().Nullable();
+        }
+
+        public override void Down()
+        {}
+    }
+}
---
+++ tools/migrator.ps1
@@ -0,0 +1,18 @@
+[CmdletBinding()]
+param(
+)
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+$PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+Write-Output $PSScriptRoot
+
+#$FluentMigratorExePath = Join-Path $PSScriptRoot \ssl.pfx
+$FluentMigratorExePath = (dir $$PSScriptRoot\..\..\packages\FluentMigrator.Tools.*\tools\AnyCPU\40\Migrate.exe | select -first 1 -expand FullName)
+$QuoteFlowDLLPath = (dir $$PSScriptRoot\..\..\QuoteFlow\bin\QuoteFlow.dll | select -first 1 -expand FullName)
+
+Write-Output $FluentMigratorExePath
+Write-Output $QuoteFlowDLLPath
+
+& $FluentMigratorExePath /connection "Data Source=(local);Initial Catalog=QuoteFlow_Migrator;Integrated Security=SSPI;" /provider sqlserver2012 /assembly $QuoteFlowDLLPath
