a0b5ee513fcbd4feecb7f3f6d71703da49c98f8e
Josh Sullivan <jaysche.sullivan@gmail.com>
2014-08-07 15:38:50 -0400

    Added manufacturer routes to the client-side routes list
    Asset details page uses the new manfuacturer route

--- QuoteFlow/Content/js/app/components/routes.js
+++ QuoteFlow/Content/js/app/components/routes.js
@@ -1,8 +1,8 @@
 ﻿QuoteFlow.Routes = {
     
-    ///////////////////////
+    ///////////////////////////
     /// ASSET ROUTES
-    //////////////////////
+    ///////////////////////////
     asset: function (routeValues) {
         var assetId = routeValues.hash.id;
         var assetName = routeValues.hash.name;
@@ -15,5 +15,22 @@
         var assetName = routeValues.hash.name;
 
         return '{0}asset/{1}/{2}/edit'.f(QuoteFlow.RootUrl, assetId, QuoteFlow.Utilities.urlFriendly(assetName));
+    },
+
+    ///////////////////////////
+    /// MANUFACTURER ROUTES
+    //////////////////////////
+    manufacturer: function(routeValues) {
+        var manufacturerId = routeValues.hash.id;
+        var manufacturerName = routeValues.hash.name;
+
+        return '{0}manufacturer/{1}/{2}'.f(QuoteFlow.RootUrl, manufacturerId, QuoteFlow.Utilities.urlFriendly(manufacturerName));
+    },
+
+    editManufacturer: function(routeValues) {
+        var manufacturerId = routeValues.hash.id;
+        var manufacturerName = routeValues.hash.name;
+
+        return '{0}manufacturer/{1}/{2}/edit'.f(QuoteFlow.RootUrl, manufacturerId, QuoteFlow.Utilities.urlFriendly(manufacturerName));
     }
 }
