7e7ff56a5776a69b3962bb785304492215cb1b31
Josh Sullivan <jaysche.sullivan@gmail.com>
2014-11-17 22:21:37 -0500

    Added token filter functions for language parsing

---
+++ QuoteFlow/Models/Assets/Index/Analyzer/TokenFilters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Snowball;
+using QuoteFlow.Infrastructure.Lucene;
+using SF.Snowball.Ext;
+
+namespace QuoteFlow.Models.Assets.Index.Analyzer
+{
+    public class TokenFilters
+    {
+        public class English
+        {
+            public class Stemming
+            {
+                public static readonly Func<TokenStream, SnowballFilter> Aggressive = stream => new SnowballFilter(stream, new EnglishStemmer());
+
+//                public static Func<TokenStream, TokenStream> Moderate = stream => new KStemFilter(stream);
+//
+//                public static Func<TokenStream, TokenStream> Minimal = stream => new EnglishMinimalStemFilter(stream); 
+
+            }
+
+            public class StopWordRemoval
+            {
+                public static readonly Func<TokenStream, TokenStream> DefaultSet = stream => new StopFilter(true, stream, StopAnalyzer.ENGLISH_STOP_WORDS_SET);
+            }
+        }
+    }
+}
---
+++ QuoteFlow/Models/Assets/Index/QuoteFlowAnalyzer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuoteFlow.Models.Assets.Index
+{
+    public class QuoteFlowAnalyzer
+    {
+    }
+}
