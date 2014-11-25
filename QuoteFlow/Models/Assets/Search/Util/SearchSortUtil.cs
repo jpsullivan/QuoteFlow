be7fa8e7b5fbd90af3c3af9518906b6558c13447
Josh Sullivan <jaysche.sullivan@gmail.com>
2014-11-25 12:24:27 -0500

    Added a search request class and a util class for sorting searches

---
+++ QuoteFlow/Models/Assets/Search/Util/SearchSortUtil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using QuoteFlow.Models.Search.Jql.Query;
+using QuoteFlow.Models.Search.Jql.Query.Order;
+
+namespace QuoteFlow.Models.Assets.Search.Util
+{
+    public class SearchSortUtil : ISearchSortUtil
+    {
+        public IList<SearchSort> mergeSearchSorts(User user, ICollection<SearchSort> newSorts, ICollection<SearchSort> oldSorts, int maxLength)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SearchSort> getSearchSorts(Query query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IOrderBy getOrderByClause(IDictionary parameterMap)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<SearchSort> concatSearchSorts(ICollection<SearchSort> newSorts, ICollection<SearchSort> oldSorts, int maxLength)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> getSearchSortDescriptions(SearchRequest searchRequest, User searcher)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
