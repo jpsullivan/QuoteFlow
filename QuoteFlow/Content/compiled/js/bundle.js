62d1d5677a00927f2641948f28536a3257d5fb47
Josh Sullivan <jaysche.sullivan@gmail.com>
2015-02-24 14:54:29 -0500

    Completed the upgrade to AUI 5.8.7

--- QuoteFlow/Content/compiled/js/bundle.js
+++ QuoteFlow/Content/compiled/js/bundle.js
@@ -1971,46 +1971,46 @@ var AssetModule = Marionette.Module.extend({
     },
 
     /**
-     * Equivalent of IssueNavInit.js
+     * Equivalent of AssetNavInit.js
      */
     init: function() {
         
     },
 
     startMediator: function (options) {
-        this.searchPageModule = new SearchPageModule({}, {
-            initialAssetTableState: options.initialAssetTableState
-        });
-        this.searchPageModule.registerViewContainers({
-            assetContainer: $(".asset-container"),
-            searchContainer: $('#.navigator-container')
-        });
-
-        // init modules
-        this.searchHeaderModule = new SearchHeaderModule({ searchPageModule: this.searchPageModule });
-        var queryModule = QueryComponent.create({
-            el: $el.find("form.navigator-search"),
-            searchers: options.initialSearcherCollectionState,
-            preferredSearchMode: "basic",
-            layoutSwitcher: true,
-            autocompleteEnabled: undefined,
-            basicAutoUpdate: true
-        });
-
-//        JIRA.bind(JIRA.Events.ISSUE_TABLE_REORDER, function (e) {
-//            if (!JIRA.Issues.Application.request("issueEditor:canDismissComment")) {
-//                e.preventDefault();
-//            }
+//        this.searchPageModule = new SearchPageModule({}, {
+//            initialAssetTableState: options.initialAssetTableState
 //        });
-
-        this.layoutSwitcherView = new LayoutSwitcher({ searchPageModule: this.searchPageModule });
-        this.layoutSwitcherView.setElement($el.find("#layout-switcher-toggle")).render();
-
-        this.controller = new AssetController();
-        return new AssetRouter({
-            controller: this.controller,
-            searchPageModule: this.searchPageModule
-        });
+//        this.searchPageModule.registerViewContainers({
+//            assetContainer: $(".asset-container"),
+//            searchContainer: $('#.navigator-container')
+//        });
+//
+//        // init modules
+//        this.searchHeaderModule = new SearchHeaderModule({ searchPageModule: this.searchPageModule });
+//        var queryModule = QueryComponent.create({
+//            el: $el.find("form.navigator-search"),
+//            searchers: options.initialSearcherCollectionState,
+//            preferredSearchMode: "basic",
+//            layoutSwitcher: true,
+//            autocompleteEnabled: undefined,
+//            basicAutoUpdate: true
+//        });
+//
+////        JIRA.bind(JIRA.Events.ISSUE_TABLE_REORDER, function (e) {
+////            if (!JIRA.Issues.Application.request("issueEditor:canDismissComment")) {
+////                e.preventDefault();
+////            }
+////        });
+//
+//        this.layoutSwitcherView = new LayoutSwitcher({ searchPageModule: this.searchPageModule });
+//        this.layoutSwitcherView.setElement($el.find("#layout-switcher-toggle")).render();
+//
+//        this.controller = new AssetController();
+//        return new AssetRouter({
+//            controller: this.controller,
+//            searchPageModule: this.searchPageModule
+//        });
     }
 });
 
