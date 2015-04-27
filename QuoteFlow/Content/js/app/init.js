34a925e64fae70cda115d18901682f75a2ccbee5
Josh Sullivan <jaysche.sullivan@gmail.com>
2015-04-27 12:28:10 -0400

    Updated npm libs to their latest versions
    Except for the handlebars one which has issues

--- QuoteFlow/Content/js/app/init.js
+++ QuoteFlow/Content/js/app/init.js
@@ -2,7 +2,6 @@
 
 window.$ = window.jQuery = require('jquery');
 var aui = require('aui');
-var Define = require('define');
 var auiExperimental = require('aui-experimental');
 var App = require('./app');
 
