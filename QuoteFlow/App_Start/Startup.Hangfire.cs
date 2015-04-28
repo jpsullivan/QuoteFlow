d55a48814227d68840ea207f08a9dc64d8bd3f0d
Josh Sullivan <jaysche.sullivan@gmail.com>
2015-04-28 01:32:27 -0400

    Increased HangFire timeout period
    Used for long-running tasks such as reindexing Lucene indexes

--- QuoteFlow/App_Start/Startup.Hangfire.cs
+++ QuoteFlow/App_Start/Startup.Hangfire.cs
@@ -1,4 +1,6 @@
-﻿using Hangfire;
+﻿using System;
+using Hangfire;
+using Hangfire.SqlServer;
 using Owin;
 using QuoteFlow.Core.DependencyResolution;
 
@@ -9,7 +11,11 @@ namespace QuoteFlow
         public void ConfigureHangfire(IAppBuilder app)
         {
             GlobalConfiguration.Configuration.UseNinjectActivator(HangfireContainer.Kernel);
-            GlobalConfiguration.Configuration.UseSqlServerStorage("QuoteFlow.SqlServer");
+
+            // background indexing can take a while, 2 hours should be plenty of time
+            var sqlServerOpts = new SqlServerStorageOptions {InvisibilityTimeout = TimeSpan.FromHours(2)};
+            GlobalConfiguration.Configuration.UseSqlServerStorage("QuoteFlow.SqlServer", sqlServerOpts);
+
             GlobalConfiguration.Configuration.UseElmahLogProvider();
 
             app.UseHangfireDashboard();
