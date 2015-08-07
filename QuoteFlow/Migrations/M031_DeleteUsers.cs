fe2708648685e565cb79aab5092fdbb2d5c2bea7
Josh Sullivan <jaysche.sullivan@gmail.com>
2015-08-07 01:19:56 -0400

    Delete whatever nuget packages were already added

---
+++ QuoteFlow/Migrations/M031_DeleteUsers.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+
+namespace QuoteFlow.Migrations
+{
+    [Migration(201508070116)]
+    public class M031_DeleteUsers : Migration
+    {
+        public override void Up()
+        {
+            
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
